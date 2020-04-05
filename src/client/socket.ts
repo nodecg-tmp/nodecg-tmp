48fd8980519b2be89e1b9b570ac04729a5fb416f
Alex Van Camp <vancamp.alex@gmail.com>
2020-04-05 11:28:47 -0500

    asdf

--- src/client/socket.ts
+++ src/client/socket.ts
@@ -9,11 +9,9 @@ declare global {
 }
 
 const params = new URLSearchParams(location.search);
-
 window.token = params.get('key') ?? Cookies.get('socketToken');
-
 if (window.token) {
-	window.socket = io(undefined, { query: { token: window.token } });
+	window.socket = io((undefined as unknown) as string, { query: { token: window.token } });
 } else {
 	window.socket = io();
 }
